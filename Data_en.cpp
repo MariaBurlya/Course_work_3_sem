cc357a0a0e3509b9779cd489b750e2fe1753ba5a
Marrrrrusya <166085136+MariaBurlya@users.noreply.github.com>
2025-12-16 20:54:16 +0700

    Update Data_en.cpp

--- Data_en.cpp
+++ Data_en.cpp
@@ -33,23 +33,29 @@ bool Date::isValidDate(int d, int m, int y) const {
 int Date::toDays() const {
     int totalDays = 0;
     
+    // Дни за полные годы
     for (int i = 0; i < year; i++) {
         totalDays += isLeapYear(i) ? 366 : 365;
     }
     
+    // Дни за полные месяцы текущего года
     for (int i = 1; i < month; i++) {
         totalDays += daysInMonth(i, year);
     }
     
+    // Дни текущего месяца
     totalDays += day - 1;
+    
     return totalDays;
 }
 
 void Date::fromDays(int days) {
     if (days < 0) {
+        // Обработка отрицательных дней (даты до 01.01.0000)
         days = 0;
     }
     
+    // Находим год
     year = 0;
     while (true) {
         int daysInThisYear = isLeapYear(year) ? 366 : 365;
@@ -60,6 +66,7 @@ void Date::fromDays(int days) {
         year++;
     }
     
+    // Находим месяц
     month = 1;
     while (true) {
         int daysInThisMonth = daysInMonth(month, year);
@@ -70,7 +77,8 @@ void Date::fromDays(int days) {
         month++;
     }
     
-    day = days + 1;
+    // Находим день
+    day = days + 1; // +1 потому что дни начинаются с 1
 }
 
 Date::Date() {
